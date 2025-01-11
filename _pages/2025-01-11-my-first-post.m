9c86ca611fe2f334e8bfbb178110838e088f0e42
DylJames <152208291+DylJames@users.noreply.github.com>
2025-01-11 14:52:45 -0500

    Delete _pages/2025-01-11-my-first-post.m

--- _pages/2025-01-11-my-first-post.m
+++
@@ -1,8 +0,0 @@
----
-layout: single
-title: "My First Post"
-date: 2025-01-11
-categories: [blog]
-tags: [jekyll, minimal-mistakes]
----
-This is my first post on my site using the Minimal Mistakes theme!
